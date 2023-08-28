341e8411dc8b9f2be132803427f2dc87a7304bb9
ElBastoh <143352162+ElBastoh@users.noreply.github.com>
2023-08-28 23:46:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const questionBtns = document.querySelectorAll(".question-btn");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const slideOutMenu = document.querySelector(".slide-out-menu");
 
-    questionBtns.forEach(function(btn) {
-        btn.addEventListener("click", function() {
-            const answer = this.nextElementSibling;
-            answer.classList.toggle("show-answer");
-        });
+    menuToggle.addEventListener("click", function() {
+        slideOutMenu.classList.toggle("menu-open");
+    });
+
+    // Schließen des Slide-Out-Menüs bei Klick außerhalb des Menüs
+    document.addEventListener("click", function(event) {
+        if (!slideOutMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+            slideOutMenu.classList.remove("menu-open");
+        }
     });
 });
